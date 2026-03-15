94e34658d7fad88e769ef013caad7fa591de5d08
gracelovell <grubylovell@gmail.com>
2026-03-14 17:00:59 -0700

    added injector parameters

--- analysis/control_dev/init_engine_params.m
+++ analysis/control_dev/init_engine_params.m
@@ -7,13 +7,21 @@ P_amb = 14.7; % [psi] ambient pressure
 V_c = 0.2; % [m^3] chamber volume
 A_t = 0.002; % [m^2] throat area
 
-% Feed Pressures
-P_feed_f = 500; % [Psi] fuel feed pressure
-P_feed_ox = 500;% [Psi] ox feed pressure
+% Injector Parameters
+V_plenum_f = 0.02; % [m^3] fuel plenum volume
+V_plenum_ox = 0.02; % [m^3] ox plenum volume
+CdA_inj_f = 0.7; % CdA for fuel injector ??need to model better than constant??
+CdA_inj_ox = 0.7; % CdA for fuel injector ??need to model better than constant??
 
-% Liquid Propellant Densities
-rho_f = 789; % [kg/m^3]
-rho_ox = 750; % [kg/m^3] get temp to get correct rho
+% Feed Pressures
+P_tank_f = 500; % [Psi] fuel feed pressure, constant because regulated with N2
+P_tank_ox = 500; % [Psi] ox feed pressure, constant because regulated with N2
+
+% Liquid Propellant Properties
+rho_f = 789; % [kg/m^3] ethanol density
+rho_ox = 750; % [kg/m^3] ??const T before combustion chamber?? liquid N2O density, temp depedent
+a_liq_f = 1144; % [m/s] speed of sound in liquid ethanol, source: Link #8
+a_liq_ox = 263; % [m/s] ??varies with temp?? speed of sound in liquid nitrous oxide
 
 % Set nominal DeltaP and mdot
 DeltaP_nom = 70; % [psi]
@@ -25,11 +33,11 @@ mdot_nom = 0.1; % [psi]
 % Fuel:
 C_d_f = 0.7; % starting estimate
 A_eff_f = mdot_nom / (C_d_f * sqrt(2 * rho_f * DeltaP_nom));
-CdA_max_f = C_d_f * A_eff_f;
+CdA_max_valve_f = C_d_f * A_eff_f;
 % Ox:
 C_d_ox = 0.7; % starting estimate
 A_eff_ox = mdot_nom / (C_d_ox * sqrt(2 * rho_ox * DeltaP_nom));
-CdA_max_ox = C_d_ox * A_eff_ox;
+CdA_max_valve_ox = C_d_ox * A_eff_ox;
 
 
 
