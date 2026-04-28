413c7bbd3ef4909bdc721a8890f9f2a0189fcfd4
gracelovell <grubylovell@gmail.com>
2026-04-28 14:19:24 -0700

    Update pid.py
    update pid.py

--- software/src/deep_thrott_code/control/pid.py
+++ software/src/deep_thrott_code/control/pid.py
@@ -5,16 +5,17 @@ class PID:
     Basic PID class
     """
 
-    def __init__(self, kp, ki, kd, reference, current_value, previous_value, dt):
+    def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
         self.kd = kd
+
+    def PID_output(self, reference, current_value, previous_value, dt):
         self.reference = reference
         self.current_value = current_value
         self.previous_value = previous_value
         self.dt = dt
-
-    def PID_output(self):
+        
         error = self.reference - self.current_value
         integral_error += error * self.dt
         derivative_error = (self.current_value - self.previous_value)/self.dt
