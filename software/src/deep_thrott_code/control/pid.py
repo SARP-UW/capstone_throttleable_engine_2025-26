ca2835d97a18298382d0a484538b9ee572e9d994
gracelovell <grubylovell@gmail.com>
2026-04-28 14:14:45 -0700

    write pid class draft1

--- software/src/deep_thrott_code/control/pid.py
+++ software/src/deep_thrott_code/control/pid.py
@@ -5,5 +5,19 @@ class PID:
     Basic PID class
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, kp, ki, kd, reference, current_value, previous_value, dt):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.reference = reference
+        self.current_value = current_value
+        self.previous_value = previous_value
+        self.dt = dt
+
+    def PID_output(self):
+        error = self.reference - self.current_value
+        integral_error += error * self.dt
+        derivative_error = (self.current_value - self.previous_value)/self.dt
+        PID_output = self.kp * error + self.ki * integral_error + self.kd * derivative_error
+
+        return PID_output
