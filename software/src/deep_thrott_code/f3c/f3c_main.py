7a09aa3fa6dfa18514fcd2222534ccf9371aa169
keiramorrissey <karenmm@uw.edu>
2026-05-06 19:39:08 -0700

    functionality for laptop test

--- software/src/deep_thrott_code/f3c/f3c_main.py
+++ software/src/deep_thrott_code/f3c/f3c_main.py
@@ -12,9 +12,10 @@ import threading
 
 # single valve actuation test
 
-pin = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+if GPIO_AVAILABLE:
+    pin = 21
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
 
 test_command_queue = Queue()
 test_ack_queue = Queue()
@@ -56,13 +57,10 @@ if GPIO_AVAILABLE:
     GPIO.output(pin, GPIO.LOW)
 
     time.sleep(5)
+    GPIO.cleanup()
 
 # time.sleep(10)
 
 # print("Single valve actuation command to controller: pulse for 10 seconds")
 # test_command_queue.put("single valve pulse", "test valve", )
 # time.sleep(10)
-
-
-
-GPIO. cleanup()
