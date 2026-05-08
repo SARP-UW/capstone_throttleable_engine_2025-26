2a10a4273a8ff5adb1c32aca6028df25ca3e6ea3
gracelovell <grubylovell@gmail.com>
2026-05-08 01:32:23 -0700

    notate units

--- analysis/control_dev/First Princ + Data Driven/cstar_lookup_table.m
+++ analysis/control_dev/First Princ + Data Driven/cstar_lookup_table.m
@@ -1,9 +1,10 @@
 % Unit conversions
 psi_to_Pa = 6894.76;
 
-Pc_vec = psi_to_Pa*readmatrix('cstar_lookup_table.xlsx','Range','B3:B23');
+Pc_vec = psi_to_Pa*readmatrix('cstar_lookup_table.xlsx','Range','B3:B23'); % [Pa]
 
 OF_vec = readmatrix('cstar_lookup_table.xlsx','Range','C2:I2');
 
 
-cstar_matr = readmatrix('cstar_lookup_table.xlsx','Range','C3:I23');
+cstar_matr = readmatrix('cstar_lookup_table.xlsx','Range','C3:I23'); % [m/s]
+
