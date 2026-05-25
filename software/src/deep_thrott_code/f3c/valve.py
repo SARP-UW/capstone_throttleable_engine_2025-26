82baaae4f06e236b62009821c840cc8066061f70
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2026-05-25 16:41:30 -0700

    testing water valves

--- software/src/deep_thrott_code/f3c/valve.py
+++ software/src/deep_thrott_code/f3c/valve.py
@@ -319,17 +319,3 @@ class WaterValvePWM:
 
     def close(self):
         self.set_angle(0.0)
-
-
-print("TESTING")
-PWM_PIN = 12
-print("Testing WaterValvePWM class")
-print(f"Using pin: {PWM_PIN}")
-water_valve = WaterValvePWM("water_valve", PWM_PIN)
-time.sleep(1)
-
-while True:
-    water_valve.open()
-    time.sleep(5)
-    water_valve.close()
-    time.sleep(5)
