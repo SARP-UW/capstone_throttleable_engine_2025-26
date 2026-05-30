1851bcbc539946f50507dc16fff66baa91a6a7ed
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2026-05-29 22:30:08 -0700

    adding prints)

--- software/src/deep_thrott_code/f3c/uart_test.py
+++ software/src/deep_thrott_code/f3c/uart_test.py
@@ -9,10 +9,10 @@ pi.set_mode(TX_ENABLE_PIN, pigpio.OUTPUT)
 
 try:
     while True:
-        print("Pulling ts shit high")
+        print(f"Pulling ts shit ({TX_ENABLE_PIN}) high")
         pi.write(TX_ENABLE_PIN, 1) # pull ts shit high
         time.sleep(1)
-        print("Pulling ts shit low")
+        print(f"Pulling ts shit ({TX_ENABLE_PIN}) low")
         pi.write(TX_ENABLE_PIN, 0) # pull ts shit low
         time.sleep(1)
 except KeyboardInterrupt:
