257fed6a604e8211d21e611fe136c17ebb79e937
keiramorrissey <karenmm@uw.edu>
2026-06-01 02:14:49 -0700

    fmctv calib

--- software/src/deep_thrott_code/f3c/valve.py
+++ software/src/deep_thrott_code/f3c/valve.py
@@ -173,9 +173,9 @@ class ThrottleValve():
             # angles calibrated for fmctv
             else:
                 if new_state == ValveState.OPEN:
-                    self.throttle(90.0, actuation_time)
+                    self.throttle(80.0, actuation_time)
                 else:
-                    self.throttle(0.0, actuation_time)
+                    self.throttle(-10.0, actuation_time)
 
     def pulse_valve(self, dt: float):
         if self.state == ValveState.CLOSED:
